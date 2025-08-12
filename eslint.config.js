0a8587357fc78a1fabd97fa8c67e9f63e24f36f3
farellano <ferminarellano.hn@gmail.com>
2025-08-12 16:49:45 -0600

    chore: add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts', 'examples/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      // TypeScript specific rules
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      
+      // General rules
+      'no-console': 'off', // Allow console for demo purposes
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-expressions': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      
+      // Style rules
+      'indent': ['error', 2],
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': ['error', 'always']
+    }
+  },
+  {
+    ignores: [
+      'dist/',
+      'node_modules/',
+      '*.js',
+      '*.d.ts',
+      'coverage/',
+      'tests/'
+    ]
+  }
+];
